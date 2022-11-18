3514f7b0b45295300f5dd9efb2d92e699071469e
rakeshindukuri <indukuri.r@northeastern.edu>
2022-11-18 13:14:26 -0500

    introduces functions for programs

---
+++ bioinfo-python-basic-scripts/dna_to_codon_conversion.py
@@ -0,0 +1,35 @@
+table = {
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+    }
+
+def conv_to_protein(dna):
+    if len(dna) % 3 == 0:
+        protein = ''.join(table[dna[i:i+3]] for i in range(0, len(dna), 3))
+    else:
+        protein = "This cannot be a protein sequence. please check your query"
+    return protein
+
+def main():
+    #dna = "ATTCATCGCTGGAGGCGGTGGCGCTTGTTGTTCTATTTGATAAGGCCTTTGGCCTTAAATAGGTCACGCATGAGACCCTTGATGTAGCGAATCTCACGC"
+    dna = input("Please enter the DNA string to convert to protein sequence:\n")
+    protein_seq = conv_to_protein(dna)
+    print(f"DNA query sequence: {dna}")
+    print(f"Protein sequence is: {protein_seq}")
+
+if __name__=="__main__":
+    main()
